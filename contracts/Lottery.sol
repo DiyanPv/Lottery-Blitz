79984232aaaac475675761fe1e7de2d6eb6f1e0b
DiyanPv <diyanpetrov61@gmail.com>
2022-12-19 16:36:25 +0200

    fixed compiler code/added helper config

--- contracts/Lottery.sol
+++ contracts/Lottery.sol
@@ -13,11 +13,7 @@ error Raffle__TransferFailed();
 error Raffle__SendMoreToEnterRaffle();
 error Raffle__RaffleNotOpen();
 error Raffle_NotEnoughETHEntered();
-/**@title A sample Raffle Contract
- * @author Patrick Collins
- * @notice This contract is for creating a sample raffle contract
- * @dev This implements the Chainlink VRF Version 2
- */
+
 contract Raffle is VRFConsumerBaseV2, AutomationCompatibleInterface {
     /* Type declarations */
     enum RaffleState {
